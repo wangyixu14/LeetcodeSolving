1056c9d838441c9668d2cebcb5c2f3777b825f0b
Yixuan Wang <34372413+wangyixu14@users.noreply.github.com>
2022-03-25 18:19:08 -0500

    Time: 8 ms (42.57%), Space: 9 MB (19.38%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,15 @@
+bool comp(vector<int> &a, vector<int> b){
+    return (a[0] - a[1]) < (b[0] - b[1]); 
+}
+
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin(), costs.end(), comp);
+        int n = costs.size() / 2, res = 0;
+        for(int i = 0; i < n; i++){
+            res += costs[i][0] + costs[i+n][1];
+        }
+        return res;
+    }
+};
