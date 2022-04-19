ffd19afc7fc4f72f93ce3a536b32fc7de08b4ef4
Yixuan Wang <34372413+wangyixu14@users.noreply.github.com>
2022-04-19 15:45:52 -0500

    Time: 35 ms (21.39%), Space: 17.2 MB (11.09%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -10,8 +10,10 @@ public:
         
         for(int i= 0; i < s.size() + 1; i++){
             for(int j = 0; j < i; j++){
-                // cout << j << " " << i <<s.substr(j, i) << flag[i] << endl;
-                if(mp[s.substr(j, i-j)] && flag[j]) flag[i] = true;
+                if(mp[s.substr(j, i-j)] && flag[j]) {
+                    flag[i] = true;
+                    break;
+                }
             }
         }
         return flag.back();
