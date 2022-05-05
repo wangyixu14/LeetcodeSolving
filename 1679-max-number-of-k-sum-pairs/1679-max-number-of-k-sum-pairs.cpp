a32076b3840ff1195c1f00bab4956ea2cf77b9aa
Yixuan Wang <34372413+wangyixu14@users.noreply.github.com>
2022-05-04 21:51:51 -0500

    Time: 277 ms (18.85%), Space: 74.2 MB (7.08%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -6,17 +6,15 @@ public:
         for(auto a : nums) {
             mp[a]++;
         }
-        for(auto m=mp.begin(); m!=mp.end(); ++m){
-            // cout << m->first << " "  << m->second << endl;
-            if(k - m->first == m->first) {
-                res += (m->second / 2);
-                // cout << m.first << endl;
+        for(auto it=mp.begin(); it!=mp.end(); ++it){
+            if(k - it->first == it->first) {
+                res += (it->second / 2);
             }
             else{
-                int num = mp[k - m->first];
-                res += min(m->second, num);
-                mp[m->first] = 0;
-                mp[k - m->first] = 0;
+                int num = mp[k - it->first];
+                res += min(it->second, num);
+                mp[it->first] = 0;
+                mp[k - it->first] = 0;
             }
         }
         return res;
