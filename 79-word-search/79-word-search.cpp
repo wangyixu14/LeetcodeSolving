5416b84a60793c4bdcff8152f5c2f12e8849aaa0
Yixuan Wang <34372413+wangyixu14@users.noreply.github.com>
2022-07-05 10:52:33 -0700

    Time: 1488 ms (5.02%), Space: 7.9 MB (91.40%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool dfs(vector<vector<char>> & board, string s, int i, int j){
-        if(!s.size()) return true;
+        if(s.size() == 0) return true;
         if(i < 0 or i >= board.size() or j < 0 or j >= board[0].size() or board[i][j] != s[0]) return false;
+        
         char tem = board[i][j];
-        board[i][j] = '*';
+        board[i][j] = '#';
         s = s.substr(1);
-        bool res = dfs(board, s, i+1, j) or dfs(board, s, i-1, j) or dfs(board, s, i, j+1) or dfs(board, s, i, j-1);
+        bool res = dfs(board, s, i+1, j) or dfs(board, s, i, j+1) or dfs(board, s, i-1, j) or dfs(board, s, i, j-1);
         board[i][j] = tem;
         return res;
     }
